2efd2840443b6e88fb296ba5904f649ba9614ee9
Dilushan <dilushan@meetrix.io>
2025-11-20 11:09:27 +0530

    Add additional Chromium flags for Microsoft Teams compatibility

--- src/lib/chromium.ts
+++ src/lib/chromium.ts
@@ -82,6 +82,10 @@ async function createBrowserContext(url: string, correlationId: string, botType:
     '--enable-features=MediaRecorder',
     '--enable-audio-service-out-of-process',
     '--autoplay-policy=no-user-gesture-required',
+    '--no-default-browser-check',
+    '--disable-crash-reporter',
+    '--disable-breakpad',
+    '--disable-infobars',
   ];
 
   // Fake device args - only for Microsoft Teams
